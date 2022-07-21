a27b9734360a109353f59e7f03a2d164f815a7fd
HollyLgru <H.grudovich@zollege.com>
2022-07-21 13:06:52 -0500

    one more video left

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super()
+    // this method SUPERCEDES the parent element
+
+    this.state = {
+      isClicked: true,
+      inputValue: "",
+      listOfTodos: []
+
+    }
+  }
+
+  handleClick = () => {
+    this.state.isClicked ? 
+      this.setState({isClicked : false}) : 
+        this.setState({isClicked : true})
+  }
+
+  handleChange = (event) => {
+    this.setState({inputValue : event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({listOfTodos: [...this.state.listOfTodos, this.state.inputValue]});
+    this.setState({inputValue: ""})
+  }
+
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Holly Grudovich
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.inputValue} onChange={this.handleChange}></input>
+          <button type="submit">Submit</button>
+          </form>
+          <p>
+            {this.state.isClicked ? "true" : "false"}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
